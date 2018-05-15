08d2c3ceb7874ac94313580e04b11e15ff0acbbe
Collin Sherman <shermanc19@mail.wlu.edu>
2018-05-15 13:42:33 -0400

    Updated MainActivity/main.xml.

--- app/src/main/java/com/example/hornsby/spacegame/MainActivity.java
+++ app/src/main/java/com/example/hornsby/spacegame/MainActivity.java
@@ -31,7 +31,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spot = findSpot(ship.getHori());
+        ship = new Ship();
+
+        mSpot1 = (ImageView) findViewById(R.id.spot_1);
+        mSpot2 = (ImageView) findViewById(R.id.spot_2);
+        mSpot3 = (ImageView) findViewById(R.id.spot_3);
+        mSpot4 = (ImageView) findViewById(R.id.spot_4);
+        mSpot5 = (ImageView) findViewById(R.id.spot_5);
+
+        findSpot(ship.getHori()).setImageResource(R.drawable.ic_ship);
 
         mSpotBar = (LinearLayout) findViewById(R.id.spot_bar);
         mSpotBar.setOnClickListener(new View.OnClickListener() {
@@ -42,39 +50,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mSpot1 = (ImageView) findViewById(R.id.spot_1);
-        mSpot2 = (ImageView) findViewById(R.id.spot_2);
-        mSpot3 = (ImageView) findViewById(R.id.spot_3);
-        mSpot4 = (ImageView) findViewById(R.id.spot_4);
-        mSpot5 = (ImageView) findViewById(R.id.spot_5);
-
         mRightButton = (Button) findViewById(R.id.right_button);
         mRightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                spot.setImageResource(R.drawable.blank);
-                ship.setHori(ship.getHori()+1);
-                spot.setImageResource(R.drawable.ic_ship);
-//                if (spot == 1) {
-//                    mSpot1.setImageResource(R.drawable.ic_launcher);
-//                    mSpot2.setImageResource(R.drawable.ic_ship);
-//                }
-//                if (spot == 2) {
-//                    mSpot2.setImageResource(R.drawable.ic_launcher);
-//                    mSpot3.setImageResource(R.drawable.ic_ship);
-//                }
-//                if (spot == 3) {
-//                    mSpot3.setImageResource(R.drawable.ic_launcher);
-//                    mSpot4.setImageResource(R.drawable.ic_ship);
-//                }
-//                if (spot == 4) {
-//                    mSpot4.setImageResource(R.drawable.ic_launcher);
-//                    mSpot5.setImageResource(R.drawable.ic_ship);
-//                }
-//                spot += 1;
-//                if (spot>=5){
-//                    spot=5;
-//                }
+                if (ship.getHori() != 5) {
+                    spot = findSpot(ship.getHori());
+                    spot.setImageDrawable(null);
+                    ship.setHori(ship.getHori() + 1);
+                    spot = findSpot(ship.getHori());
+                    spot.setImageResource(R.drawable.ic_ship);
+                }
             }
         });
 
@@ -82,30 +68,13 @@ public class MainActivity extends AppCompatActivity {
         mLeftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                spot.setImageResource(R.drawable.blank);
-                ship.setHori(ship.getHori()-1);
-                spot.setImageResource(R.drawable.ic_ship);
-
-//                if (spot == 2) {
-//                    mSpot2.setImageResource(R.drawable.ic_launcher);
-//                    mSpot1.setImageResource(R.drawable.ic_ship);
-//                }
-//                if (spot == 3) {
-//                    mSpot3.setImageResource(R.drawable.ic_launcher);
-//                    mSpot2.setImageResource(R.drawable.ic_ship);
-//                }
-//                if (spot == 4) {
-//                    mSpot4.setImageResource(R.drawable.ic_launcher);
-//                    mSpot3.setImageResource(R.drawable.ic_ship);
-//                }
-//                if (spot == 5) {
-//                    mSpot5.setImageResource(R.drawable.ic_launcher);
-//                    mSpot4.setImageResource(R.drawable.ic_ship);
-//                }
-//                spot -= 1;
-//                if (spot<=1) {
-//                    spot = 1;
-//                }
+                if (ship.getHori() != 1) {
+                    spot = findSpot(ship.getHori());
+                    spot.setImageDrawable(null);
+                    ship.setHori(ship.getHori() - 1);
+                    spot = findSpot(ship.getHori());
+                    spot.setImageResource(R.drawable.ic_ship);
+                }
             }
         });
     }
@@ -116,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
         else if (spot == 2) { shipLocation = mSpot2; }
         else if (spot == 3) { shipLocation = mSpot3; }
         else if (spot == 4) { shipLocation = mSpot4; }
-        else if (spot ==5) { shipLocation = mSpot5; }
+        else if (spot == 5) { shipLocation = mSpot5; }
         return shipLocation;
     }
 }
